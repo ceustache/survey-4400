e0576d7364006be7316803dea280fe79e508b2a7
cris <criseustache44@gmail.com>
2020-01-28 17:18:27 -0600

    OS bug fixed

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,11 @@
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require_tree .
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+
+
